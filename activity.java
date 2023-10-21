eef25bc5bfb51884177a86b81b3c9fbba521af2d
Shaineebrada <142649895+Shaynnezkie@users.noreply.github.com>
2023-10-21 10:54:28 +0800

    Add files via upload

---
+++ activity.java
@@ -0,0 +1,22 @@
+package ebaradalab3;
+
+ public class activity {
+
+public  static  void main (String [] args) {
+  int[] num =
+{11,22,33,44,55,66,77,88,99,100};
+ 
+System.out.println("Output:");
+System.out.println(num[0]);
+System.out.println(num[1]);
+System.out.println(num[2]);
+System.out.println(num[3]);
+System.out.println(num[4]);
+System.out.println(num[5]);
+System.out.println(num[6]);
+System.out.println(num[7]);
+System.out.println(num[8]);
+System.out.println(num[9]);
+
+   }
+}
